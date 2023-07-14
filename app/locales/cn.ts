dfc9d78a136c5cc075bc5f84a51f3dbdce83dc56
牛永峰 <121937449+wangchenhan3006@users.noreply.github.com>
2023-07-14 10:21:17 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -73,7 +73,7 @@ const cn = {
     Download: "下载文件",
     Share: "分享到 ShareGPT",
     MessageFromYou: "来自你的消息",
-    MessageFromChatGPT: "来自 ChatiGPT 的消息",
+    MessageFromChatGPT: "来自 ChatGPT 的消息",
     Format: {
       Title: "导出格式",
       SubTitle: "可以导出 Markdown 文本或者 PNG 图片",
